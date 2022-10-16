dc0b791f6ec4ec3d4867048f3a61a64f67e1e939
Aki <mertunctuncer@gmail.com>
2022-10-16 14:01:55 +0300

    Chest sorting implementation

--- src/main/kotlin/dev/peopo/chestsorter/SortAction.kt
+++ src/main/kotlin/dev/peopo/chestsorter/SortAction.kt
@@ -1,4 +1,46 @@
 package dev.peopo.chestsorter
 
-class SortAction {
+import org.bukkit.Material
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+
+class SortAction(private val inventory: Inventory) {
+
+	private val totalAmounts = mutableMapOf<ItemStack, Int>()
+
+	fun calculateTotals() : SortAction {
+		for(item in inventory.contents) {
+			if(item == null || item.type == Material.AIR) continue
+			var typeExists = false
+			for(cachedItem in totalAmounts.keys) {
+				if(cachedItem.isSimilar(item)) {
+					totalAmounts[cachedItem] = totalAmounts[cachedItem]!! + item.amount
+					typeExists = true
+					break
+				}
+			}
+			if(!typeExists) {
+				totalAmounts[item] = item.amount
+			}
+		}
+
+		return this
+	}
+
+
+	fun sort() {
+		inventory.clear()
+		var slot = 0
+		for((item, amount) in totalAmounts) {
+			var mutableAmount = amount
+			while(mutableAmount > 0) {
+				val amountToPlace = if(mutableAmount > item.maxStackSize) item.maxStackSize else mutableAmount
+				val itemToPlace = item.clone()
+				itemToPlace.amount = amountToPlace
+				inventory.setItem(slot, itemToPlace)
+				mutableAmount -= amountToPlace
+				slot++
+			}
+		}
+	}
 }
