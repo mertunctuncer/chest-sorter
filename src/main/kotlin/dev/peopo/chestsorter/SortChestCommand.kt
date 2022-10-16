d64314fd48d31596c4f51bb7eb9c7a1cfa22d32c
Aki <mertunctuncer@gmail.com>
2022-10-16 14:02:05 +0300

    Command impl

--- src/main/kotlin/dev/peopo/chestsorter/SortChestCommand.kt
+++ src/main/kotlin/dev/peopo/chestsorter/SortChestCommand.kt
@@ -1,4 +1,49 @@
 package dev.peopo.chestsorter
 
-class SortChestCommand {
+import org.bukkit.Material
+import org.bukkit.block.Chest
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+
+class SortChestCommand : TabExecutor{
+
+	override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+		if(sender !is Player) {
+			sender.sendMessage("Only players can use this command!")
+			return true
+		}
+		val player = sender
+
+		if(!player.hasPermission("chestsorter.sort")) {
+			config.getString("no-permission-message")?.colorize()?.let { sender.sendMessage(it) } ?: logger.severe("Config file is missing the no-permission-message!")
+			return true
+		}
+
+		val targetBlock = player.getTargetBlock(10)
+
+		if(targetBlock == null || targetBlock.isEmpty) {
+			config.getString("no-target-message")?.colorize()?.let { sender.sendMessage(it) } ?: logger.severe("Config file is missing the no-target-message!")
+			return true
+		}
+
+		if(targetBlock.type != Material.CHEST && targetBlock.type != Material.TRAPPED_CHEST) {
+			config.getString("not-chest-message")?.colorize()?.let { sender.sendMessage(it) } ?: logger.severe("Config file is missing the not-chest-message!")
+			return true
+		}
+
+		val inventory = (targetBlock.state as Chest).inventory
+
+		SortAction(inventory)
+			.calculateTotals()
+			.sort()
+
+		config.getString("inventory-sorted-message")?.colorize()?.let { sender.sendMessage(it) } ?: logger.severe("Config file is missing the inventory-sorted-message!")
+		return true
+	}
+
+	override fun onTabComplete(sender: CommandSender, command: Command, label: String, args: Array<out String>?): MutableList<String>? {
+		return null
+	}
 }
